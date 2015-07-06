69e3a625c2b3df896be57892a9c240283fb51a2b
Stan Lewis <slewis@fusesource.com>
2015-07-06 16:40:01 -0400

    Rebuild just in case :-)

--- defs.d.ts
+++ defs.d.ts
@@ -1,10 +1,10 @@
 /// <reference path="d.ts/includes.d.ts"/>
+/// <reference path="d.ts/googleOAuth/ts/googleOAuthGlobals.d.ts"/>
+/// <reference path="d.ts/googleOAuth/ts/googleOAuthHelpers.d.ts"/>
+/// <reference path="d.ts/googleOAuth/ts/googleOAuthPlugin.d.ts"/>
 /// <reference path="d.ts/keycloak/ts/keycloakGlobals.d.ts"/>
 /// <reference path="d.ts/keycloak/ts/keycloakHelpers.d.ts"/>
 /// <reference path="d.ts/keycloak/ts/keycloakPlugin.d.ts"/>
 /// <reference path="d.ts/osOAuth/ts/osOAuthGlobals.d.ts"/>
 /// <reference path="d.ts/osOAuth/ts/osOAuthHelpers.d.ts"/>
 /// <reference path="d.ts/osOAuth/ts/osOAuthPlugin.d.ts"/>
-/// <reference path="d.ts/googleOAuth/ts/googleOAuthGlobals.d.ts"/>
-/// <reference path="d.ts/googleOAuth/ts/googleOAuthHelpers.d.ts"/>
-/// <reference path="d.ts/googleOAuth/ts/googleOAuthPlugin.d.ts"/>
