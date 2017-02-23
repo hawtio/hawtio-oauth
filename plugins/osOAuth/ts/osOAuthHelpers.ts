08dd41237dabc364c6a8702ea3f05139556449da
Antonin Stefanutti <antonin@stefanutti.fr>
2017-02-23 10:54:03 +0100

    Polish and reformat

--- plugins/osOAuth/ts/osOAuthHelpers.ts
+++ plugins/osOAuth/ts/osOAuthHelpers.ts
@@ -1,7 +1,7 @@
 /// <reference path="osOAuthGlobals.ts"/>
 module OSOAuth {
 
-  var OS_TOKEN_STORAGE_KEY = 'osAuthCreds';
+  const OS_TOKEN_STORAGE_KEY = 'osAuthCreds';
 
   export function currentTimeSeconds() {
     return Math.floor(new Date().getTime() / 1000);
@@ -18,12 +18,12 @@ module OSOAuth {
   }
 
   export function doLogout(config = window['OSOAuthConfig'], userDetails = OSOAuth.userProfile) {
-    var currentURI = new URI(window.location.href);
-    var uri = new URI(config.oauth_authorize_uri);
+    const currentURI = new URI(window.location.href);
+    const uri        = new URI(config.oauth_authorize_uri);
     uri.path('/oapi/v1/oAuthAccessTokens' + userDetails.token);
     authenticatedHttpRequest({
       type: 'DELETE',
-      url: uri.toString()
+      url : uri.toString()
     }, userDetails).always(() => {
       clearTokenStorage();
       doLogin(OSOAuthConfig, {
@@ -33,44 +33,44 @@ module OSOAuth {
   }
 
   export function doLogin(config, options) {
-    var clientId = config.oauth_client_id;
-    var targetURI = config.oauth_authorize_uri;
-    var uri = new URI(targetURI);
+    const clientId  = config.oauth_client_id;
+    const targetURI = config.oauth_authorize_uri;
+    const uri       = new URI(targetURI);
     uri.query({
-      client_id: clientId,
+      client_id    : clientId,
       response_type: 'token',
-      state: options.uri,
-      redirect_uri: options.uri,
-      scope: options.scope
+      state        : options.uri,
+      redirect_uri : options.uri,
+      scope        : options.scope
     });
-    var target = uri.toString();
+    const target = uri.toString();
     log.debug("Redirecting to URI: ", target);
     window.location.href = target;
   }
 
   export function extractToken(uri) {
-    var query = uri.query(true);
+    const query = uri.query(true);
     log.debug("Query: ", query);
-    var fragmentParams:any = new URI("?" + uri.fragment()).query(true);
+    const fragmentParams: any = new URI("?" + uri.fragment()).query(true);
     log.debug("FragmentParams: ", fragmentParams);
     if (fragmentParams.access_token && (fragmentParams.token_type === "bearer") || fragmentParams.token_type === "Bearer") {
       log.debug("Got token");
-      var localStorage = Core.getLocalStorage();
-			var creds = {
-				token_type: fragmentParams.token_type,
-				access_token: fragmentParams.access_token,
-				expires_in: fragmentParams.expires_in,
-        obtainedAt: currentTimeSeconds()
+      const localStorage          = Core.getLocalStorage();
+      const creds                 = {
+        token_type  : fragmentParams.token_type,
+        access_token: fragmentParams.access_token,
+        expires_in  : fragmentParams.expires_in,
+        obtainedAt  : currentTimeSeconds()
       };
       localStorage['osAuthCreds'] = angular.toJson(creds);
-			delete fragmentParams.token_type;
-			delete fragmentParams.access_token;
-			delete fragmentParams.expires_in;
-			uri.fragment("").query(fragmentParams);
-			var target = uri.toString();
-			log.debug("redirecting to: ", target);
+      delete fragmentParams.token_type;
+      delete fragmentParams.access_token;
+      delete fragmentParams.expires_in;
+      uri.fragment("").query(fragmentParams);
+      const target = uri.toString();
+      log.debug("redirecting to: ", target);
       window.location.href = target;
-			return creds;
+      return creds;
     } else {
       log.debug("No token in URI");
       return undefined;
@@ -78,13 +78,13 @@ module OSOAuth {
   }
 
   export function clearTokenStorage() {
-    var localStorage = Core.getLocalStorage();
+    const localStorage = Core.getLocalStorage();
     delete localStorage[OS_TOKEN_STORAGE_KEY];
   }
 
   export function checkToken(uri) {
-    var localStorage = Core.getLocalStorage();
-    var answer = undefined;
+    const localStorage = Core.getLocalStorage();
+    let answer         = undefined;
     if (OS_TOKEN_STORAGE_KEY in localStorage) {
       try {
         answer = angular.fromJson(localStorage[OS_TOKEN_STORAGE_KEY]);
@@ -100,6 +100,4 @@ module OSOAuth {
     log.debug("Using creds: ", answer);
     return answer;
   }
-
-
 }
